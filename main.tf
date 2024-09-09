70ff0a33f19070bcde4fb6f96d3de0ef74df39d4
outlander <k2awsprod@gmail.com>
2024-09-09 16:02:48 +1000

    trigger run 1

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = "ap-southeast-2"  # e.g., us-west-2
+  region = "ap-southeast-2" # e.g., us-west-2
 }
 
 terraform {
   backend "s3" {
-    bucket         = "backstage-tf-state"       # S3 bucket name
-    key            = "terraform.tfstate"         # Path to the state file
-    region         = "ap-southeast-2"               # AWS region
-    encrypt        = true                        # Enable encryption
+    bucket  = "backstage-tf-state" # S3 bucket name
+    key     = "terraform.tfstate"  # Path to the state file
+    region  = "ap-southeast-2"     # AWS region
+    encrypt = true                 # Enable encryption
   }
 }
 
@@ -17,8 +17,9 @@ provider "local" {}
 variable "file_count" {
   description = "The number of files to create (1 to 3)"
   type        = number
+  default     = 1
   validation {
-    condition = var.file_count >= 1 && var.file_count <= 3
+    condition     = var.file_count >= 1 && var.file_count <= 3
     error_message = "file_count must be between 1 and 3."
   }
 }
@@ -26,6 +27,7 @@ variable "file_count" {
 variable "file_content" {
   description = "The content to add to the files"
   type        = string
+  default     = "Hello"
 }
 
 resource "local_file" "example" {
